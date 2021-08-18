2f98437571f987133db5c98b8a091a46f3991507
Carlos Andrés Sánchez <cagudeloa@uninorte.edu.co>
2021-08-18 11:24:19 -0500

    Optimize Bouncing Animation

--- lib/widgets/bouncing_button.dart
+++ lib/widgets/bouncing_button.dart
@@ -16,7 +16,6 @@ class _BouncingButtonState extends State<BouncingButton>
     with SingleTickerProviderStateMixin {
   late final Widget child;
   late final Function onPressed;
-  late double _scale;
   late AnimationController _controller;
   _BouncingButtonState({required this.child, required this.onPressed});
 
@@ -29,7 +28,7 @@ class _BouncingButtonState extends State<BouncingButton>
       ),
       lowerBound: 0.0,
       upperBound: 0.05,
-    )..addListener(() => setState(() {}));
+    );
     super.initState();
   }
 
@@ -37,19 +36,22 @@ class _BouncingButtonState extends State<BouncingButton>
   void _tapUp(TapUpDetails details) => _controller.reverse();
   @override
   Widget build(BuildContext context) {
-    _scale = 1 - _controller.value;
-    return GestureDetector(
-      onTapUp: (TapUpDetails details) async {
-        await Future.delayed(Duration(milliseconds: 50));
-        _tapUp(details);
-        onPressed();
-      },
-      onPanStart: (_) => _controller.reverse(),
-      // [USER_SCROLL]: To restore item size when it's released
-      onPanCancel: () => _controller.reverse(),
-      onTapDown: (TapDownDetails details) => _tapDown(details),
-      child: Transform.scale(
-        scale: _scale,
+    return AnimatedBuilder(
+      animation: _controller,
+      child: GestureDetector(
+        onTapUp: (TapUpDetails details) async {
+          await Future.delayed(Duration(milliseconds: 50));
+          _tapUp(details);
+          onPressed();
+        },
+        onPanStart: (_) => _controller.reverse(),
+        // [USER_SCROLL]: To restore item size when it's released
+        onPanCancel: () => _controller.reverse(),
+        onTapDown: (TapDownDetails details) => _tapDown(details),
+        child: child,
+      ),
+      builder: (_, child) => Transform.scale(
+        scale: 1 - _controller.value,
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 64.0, vertical: 14.0),
           decoration: BoxDecoration(
